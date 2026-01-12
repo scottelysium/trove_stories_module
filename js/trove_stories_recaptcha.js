9f9fdd634a2a860371ee3823b3334d9aef79c7da
scottelysium <scott.beardow@elysiumdigital.com.au>
2026-01-12 16:39:03 +1100

    banner updates

--- js/trove_stories_recaptcha.js
+++ js/trove_stories_recaptcha.js
@@ -7,9 +7,7 @@
             once('trove_stories_form', '#webform-submission-trove-story-add-form').forEach(function (trove_form_element) { //should only loop once, just dealing with array
                 
                 if (typeof grecaptcha === 'undefined') {
-                    console.log("recaptcha is not valid");
                     const recaptchaWarn = trove_form_element.querySelector('.recaptchaInvalid');
-                    console.log(recaptchaWarn);
                     recaptchaWarn.classList.add("show");
                 }
 
@@ -21,15 +19,16 @@
 
                     Why:
                     We still want the form to do a normal webform submission but we
-                    also need to fetch the recaptcha token before submission. Simply
-                    adding a submission handler on the form and calling the recaptcha code
-                    there skips over the async recaptcha api call unless we preventDefault,
+                    also need to fetch the recaptcha token on submit click but before 
+                    the actual submission process. But adding a submission handler on the 
+                    form and calling the recaptcha code there skips over the async 
+                    recaptcha api call unless we preventDefault,
                     but preventDefault then requires another form.submit() call after the 
                     recaptcha api call is complete - this causes the submission handler to run
                     again in an infinite loop or would require some messy boolean logic.
                     The solution here is to have a fake submission button that after clicking
                     grabs the recaptcha, and then call the click event on the real form submission 
-                    button. This also triggers the natural HTML5 form validation rules.
+                    button. This also triggers the organic HTML5 form validation rules.
                     */
                 const recaptchaSubmitButton = trove_form_element.querySelector('#recaptcha-submit');
                 const hiddenSubmitButton = trove_form_element.querySelector('#edit-submit');
