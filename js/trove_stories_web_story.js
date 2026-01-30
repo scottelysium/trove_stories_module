2483556f906291a87af2cdf1e17e903f89adf6dd
scottelysium <scott.beardow@elysiumdigital.com.au>
2026-01-30 16:33:50 +1100

    gallery images modal mostly done

--- js/trove_stories_web_story.js
+++ js/trove_stories_web_story.js
@@ -3,14 +3,23 @@
 
     document.addEventListener("DOMContentLoaded", function(event) {
 
-        
-        const storyGrid = document.querySelector(".story-grid");
-    
         const storySliderWrapper = document.querySelector(".story-slider-wrapper");
 
-        if (!storySliderWrapper) return;
+        if (!storySliderWrapper) return; //no images.
+
+        /* setup image modal popup functionality **/
+        const storyGalleryModal = document.getElementById("story-gallery-modal");
+        const storyGalleryModalInner = document.getElementById("story-gallery-modal-inner");
+        const modalCloseButton = document.getElementById("story-gallery-modal-close");
+        modalCloseButton.addEventListener("click", function() {
+            
+            storyGalleryModal.classList.remove("show");
+        });
+
+        
 
         /** offset gallery to align with story-grid plus 120px indent*/
+        const storyGrid = document.querySelector(".story-grid");
         storySliderWrapper.style.left = (storyGrid.offsetLeft + 120) + "px";
 
         storySliderWrapper.classList.add("show"); //makes nice fade in
@@ -19,6 +28,33 @@
         const rightButton = document.getElementById("story-button-right");
         const leftButton = document.getElementById("story-button-left");
         const storyGallerySlider = document.getElementById("story-gallery-slider");
+        const storyImageWrappers = document.querySelectorAll(".story-gallery-slide");
+
+        //setup click events for each image
+        storyImageWrappers.forEach((imgWrapper, i, elements) => {
+            imgWrapper.addEventListener("click", function(e) {
+                console.log("event click for img number " + i);
+                console.log(e.currentTarget);
+                const fullImageUrl = e.currentTarget.getAttribute('data');
+                console.log(fullImageUrl);
+                const fullImageElement = document.createElement("img");
+                fullImageElement.classList.add("modalFade");
+                fullImageElement.src = fullImageUrl;
+                storyGalleryModalInner.innerHTML = ''; //clear out previous img
+                storyGalleryModalInner.appendChild(fullImageElement);
+                //.getAttribute('data-item-id')
+                storyGalleryModal.classList.add("show");
+            });
+        });
+
+        // for (let i = 0; i < storyImageWrappers.length; i++) {
+        //     console.log(i);
+        //     storyImageWrappers[i].addEventListener("click", function() {
+        //         console.log("event click for img number " + i);
+        //     });
+        // }
+        
+
         const storyImages = document.querySelectorAll(".story-gallery-slider img");
 
         let leftEdgeImgIndex = 0;
